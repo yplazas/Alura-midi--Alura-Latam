83725ef2fbbfef7e221266fefed4a62cb70e8b25
yplazas <yeialex9511@gmail.com>
2024-10-07 17:50:20 -0500

    Ajuste: Se termina proyecto de alura Midi, agregando las funciones necesarias para que todas las teclas puedan reproducir su sonido respectivo

--- main.js
+++ main.js
@@ -1,2 +1,50 @@
-let teclaPom = document.querySelector('.tecla_pom');
-console.log(teclaPom);
+const playSonido = (idElementoAudio) => {
+    document.querySelector(idElementoAudio).play();
+};
+
+const listaTeclas = document.querySelectorAll(".tecla");
+
+// forma 1
+// let contador = 0;
+
+// while (contador < listaTeclas.length) {
+//     const tecla = listaTeclas[contador];
+//     let idAudio = `#sonido_${tecla.classList[1]}`;
+//     tecla.onclick = () => {
+//         playSonido(idAudio);
+//     };
+//     contador++;
+// }
+
+/* forma 2
+
+for (let i = 0; i < listaTeclas.length; i++) {
+    const element = listaTeclas[i];
+    element.onclick = () => {
+        playSonido(`#sonido_${element.classList[1]}`);
+    };
+}
+
+*/
+
+// forma 3
+// ciclo para recorrer cada tecla de sonido
+for (const tecla of listaTeclas) {
+    let idAudio = `#sonido_${tecla.classList[1]}`;
+    // funcion anonima para reproducir sonido
+    tecla.onclick = () => {
+        playSonido(idAudio);
+    };
+    // funcion anonima para cambiar estilo de boton "Presionado"
+    tecla.onkeydown = (evento) => {
+        if (evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add("activa");
+        }
+    };
+    // funcion anonima para cambiar estilo de boton "Sin pulsar"
+    tecla.onkeyup = (evento) => {
+        if (evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.remove("activa");
+        }
+    };
+}
